5c8524d85c29754a6978ecf448dbbb8a83679101
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-13 22:59:17 -0500

    created GET request to retrieve all users and associated thougths and friends

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const { ObjectId } = require('mongoose').Types;
+
+// import User model
+const { User } = require('../models')
+
+module.exports = {
+    // GET all users
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find()
+            .select('-__v')
+            .populate('thoughts')
+            .populate('friends');
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
