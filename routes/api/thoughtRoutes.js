a1b5597499db12a8b52822b586631a2e57d424f2
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-13 21:59:53 -0500

    added route endpoints for thoughts and reactions model

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,3 +11,16 @@ const {
     removeReaction,
 } = require('../../controllers/courseController');
 
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
