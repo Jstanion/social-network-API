b251f5241c785b42f608dc09a6a0dd185ea0c701
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-14 13:59:36 -0500

    added controller for single user retrieval

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,7 @@
 const User = require('../models/User');
 
-// import User model
-const { User } = require('../models')
-
 module.exports = {
+
     // GET all users
     async getAllUsers(req, res) {
         try {
@@ -16,4 +14,21 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async getUserById(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('thoughts')
+            .populate('friends');
+            
+            if (!user) {
+                return res.status(404).json({ message: 'User not found!' });
+            }
+            
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async 
 }
