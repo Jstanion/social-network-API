698997b3f5620f097138c80024c99cd871ebeb0c
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-14 14:17:33 -0500

    changed filepath for thoughtRoutes importing error

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,18 +9,25 @@ const {
     deleteThought,
     addReaction,
     removeReaction,
-} = require('../controllers/thoughtController');
+} = require('../../controllers/thoughtController');
 
 // /api/thoughts
-router.route('/').get(getAllThoughts).post(createThought);
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction)
+router.route('/:thoughtId/reactions')
+    .post(addReaction)
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/thoughtId/reactions/:reactionId').delete(removeReaction);
+router.route('/thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,19 +5,26 @@ const {
     getAllUsers,
     getUserById,
     createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    removeFriend
-} = require('../controllers/userController.js');
+    // updateUser,
+    // deleteUser,
+    // addFriend,
+    // removeFriend
+} = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getAllUsers).post(createUser);
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+    .get(getUserById)
+    // .put(updateUser)
+    // .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId')
+    // .post(addFriend)
+    // .delete(removeFriend);
 
 module.exports = router;
