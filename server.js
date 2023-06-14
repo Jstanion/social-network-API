71c227df4d6e2c8ed8c0771287a239449c8cb4fc
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-14 13:32:21 -0500

    set the connection utlities and added middleware to parse data

---
+++ server.js
@@ -0,0 +1,28 @@
+// importing the express module for creating a server
+const express = require('express');
+
+// importing the db module to connect to the database
+const db = require('./config/connection');
+
+// importing the routes module for handling API routes
+const routes = require('./routes');
+
+// setting the port number for the server
+const PORT = 3001;
+
+// creating an instance of the express application
+const app = express();
+
+// configuring the middleware to parse URL-encoded and JSON request bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// attaching the routes middleware to the application
+app.use(routes);
+
+// establishing a database connection and strating up the server
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+});
