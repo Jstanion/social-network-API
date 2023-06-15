ee2f7d77f184e2f47b275b497350a27b8ccff8a4
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-06-15 15:26:16 -0500

    defined GET route for retrieving a single thought by its ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought, User } = require('../models');
+const Thought = require('../models/Thought');
 
 module.exports = {
     // retrieve all thoughts
@@ -10,5 +10,18 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-  
+    // retrieve a single thought by it's ID
+    async getThoughtById(req, res) {
+        try {
+            const singleThought = await Thought.findOne({ _id: req.params.thoughtId })
+
+            if (!singleThought) {
+                return res.status(404).json({ message: 'User not found!' });
+            }
+            
+            res.json(singleThought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 }
